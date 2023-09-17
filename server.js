6d58c85044724bb2014c101704a9369200bba97b
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-09-17 15:32:16 +0300

    bootcamp routers created

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
-// Load env vars
-dotenv.config({ path: './config/config.env' });
+/*** Route files */
+const bootcamps = require("./routes/bootcamp")
+
+/*** Load env file*/
+dotenv.config({path: './config/config.env'});
 
 const app = express();
+
+/*** Mount routers */
+app.use('/api/v1/bootcamps', bootcamps);
+
 
 const PORT = process.env.PORT || 8000;
 
