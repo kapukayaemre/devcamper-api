3bdd896bcb35b84dec862902dcb511c114bc8204
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-09-17 18:18:39 +0300

    MongoDB connected, handled unhandled errors

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-/*** Route files */
-const bootcamps = require("./routes/bootcamp")
+const connectDB = require("./config/db");
 
 /*** Load env file*/
 dotenv.config({path: './config/config.env'});
 
+/*** Connect to database */
+connectDB();
+
+/*** Route files */
+const bootcamps = require("./routes/bootcamp");
+
 const app = express();
 
-/*** Dev Logging Middleware */
+/*** Dev logging middleware */
 if (process.env.NODE_ENV === "development") {
     app.use(morgan('dev'));
 }
@@ -22,8 +26,13 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 })
 
-
+/*** Handle unhandled promise rejections */
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    /**? Close server & exit process */
+    server.close(() => process.exit(1));
+});
