9a6fa17322acee7b28555e47e43e44705767d8b4
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-10-01 22:30:51 +0300

    course controller and routes created

--- routes/bootcamp.js
+++ routes/bootcamp.js
@@ -6,8 +6,15 @@ const {
     updateBootcamp,
     deleteBootcamp,
     getBootcampsInRadius
-} = require('../controllers/bootcamp')
+} = require('../controllers/bootcamp');
+
+// Include other resource routers
+const courseRouter = require('./courses');
+
 const router = express.Router();
+
+// Re-route into other resource routers
+router.use('/:bootcampId/courses', courseRouter);
 
 router
     .route('/radius/:zipcode/:distance')
