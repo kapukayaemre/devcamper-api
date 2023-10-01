555c26d38466f71f4aa74722a2599344e5c8ecc3
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-10-01 23:12:27 +0300

    populate, virtual and cascade delete added

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -2,6 +2,7 @@ const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const geocoder = require('../utils/geocoder');
 const Bootcamp = require('../models/Bootcamp');
+const Course = require('../models/Course');
 
 
 // @desc    Get all bootcamps
@@ -26,7 +27,7 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match =>  `$${match}`);
 
   // Finding resource
-  query = Bootcamp.find(JSON.parse(queryStr));
+  query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
 
   // Select Fields
   if(req.query.select) {
@@ -135,11 +136,17 @@ exports.updateBootcamp = asyncHandler(async (req, res, next) => {
 // @access  Private
 exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
 
-    const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+    const bootcamp = await Bootcamp.findById(req.params.id);
 
     if (!bootcamp) {
         return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
+
+    // Find and delete associated courses
+    await Course.deleteMany({ bootcamp: req.params.id });
+
+    // Delete the bootcamp
+    await Bootcamp.deleteOne({ _id: req.params.id });
 
     res.status(200).json({
         success: true,
